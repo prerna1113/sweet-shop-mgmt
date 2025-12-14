fdb9db61b9d247f1389b6eae9fc0bbf0e75470f3
prerna1113 <prernakumari7427@gmail.com>
2025-12-14 15:24:12 +0530

    feat(frontend): connect React app to backend API using axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+import api from './api/api';
+
 function App() {
+  useEffect(() => {
+    api.get('/sweets')
+      .then(res => console.log('Sweets from backend:', res.data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div>
       <h1>Sweet Shop Management System</h1>
-      <p>Frontend is running</p>
+      <p>Check console for backend data</p>
     </div>
   );
 }
